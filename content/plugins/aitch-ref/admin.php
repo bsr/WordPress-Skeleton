ea9e37f0bfa173ca048842290a5eabd901ce299d
Brent Rieck <bsr@spek.org>
2013-03-23 13:11:25 -0700

    added base plugins

---
+++ content/plugins/aitch-ref/admin.php
@@ -0,0 +1,23 @@
+<?
+/*
+*
+* Version: 0.75
+*/
+?>
+<div class="wrap">
+	<h2>aitch ref!</h2>
+	
+	<?= $messages; ?>
+	
+	<p>possible urls seperated by space or new line (include http/s, no trailing slash)</p>
+
+	<form method="post">
+		<textarea name="urls" cols="60" style="background-image:url(<?= $path; ?>ref.jpg);background-repeat:no-repeat;background-position:bottom right; height:377px"><?= $urls; ?></textarea>
+		
+		<a href="http://www.flickr.com/photos/avinashkunnath/2402114514/in/photostream/" style="font-size:.6em">photo by Avinash Kunnath</a>
+		
+		<div>
+			<input type="submit" value="Update"/>
+		</div>
+	</form>
+</div>
